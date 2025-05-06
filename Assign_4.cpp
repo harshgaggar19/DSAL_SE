a72705a126102386b0b235aefa4b24644e9ad2a8
Harsh Gaggar <151357589+harshgaggar19@users.noreply.github.com>
2025-05-06 16:31:56 +0530

    Update Assign_4.cpp

--- Assign_4.cpp
+++ Assign_4.cpp
@@ -1,210 +1,245 @@
-//============================================================================
-// Name        : Assignment_4.cpp
-// Author      : Harsh Gaggar
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Consider telephone book database of N clients. Make use of a hash table implementation
-//				 to quickly look up client‘s telephone number. Make use of two collision handling
-//				 techniques and compare them using number of comparisons required to find a set of
-//				 telephone numbers (Note: Use linear probing with replacement and without replacement)
-//============================================================================
+/*
+Author - Shantanu Wable
+
+Consider telephone book database of N clients. Make use of a hash table
+implementation to quickly look up client‘s telephone number. Make use of two collision
+handling techniques and compare them using number of comparisons required to find a
+set of telephone numbers
+*/
+
+// #include <bits/stdc++.h>
 #include <iostream>
-#include <bits/stdc++.h>
-
 using namespace std;
+typedef long long ll;
 
-const int tableSize = 10;
+class Client {
 
-struct Client{
-    string cname;
-    string phoneNo;
+    char name[20];
+    ll phone;
+
+public:
+
+    Client() { 
+        strcpy( name , "----" ) ;
+        phone = 0 ;
+    }
+
+    void printDetails() {
+		cout.width( 20 ) ;
+		cout << name << " " ;
+		cout.width( 20 ) ;
+		cout << phone << "\n" ;
+	}
+
+    Client( char n[] , ll telephone ) {
+        strcpy( name , n ) ;
+        phone = telephone ;
+    }
+
+    bool isEmpty(){
+        return phone == 0;
+    }
+
+    friend class Hashtable;
 };
 
-class DataBase {
-private:
-    vector<Client> linearProbing;
-    vector<Client> linearProbingWithReplacement;
-    int compProb;
-    int compWithRepl;
+class Hashtable
+{
+    int tableSize;
+    Client* clients;
 
-    int hash(int key) {
-        return (key % tableSize);
+    int hashfunction( char clientName[] )
+    {   
+        int n = sizeof(clientName)/sizeof(clientName[0]);
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=int(clientName[i]);
+        return sum%n;
+    }
+
+    bool isOriginal( int index ) {
+        return hashfunction( clients[index].name ) == index;
     }
 
 public:
-    DataBase() {
-        linearProbing.resize(tableSize);
-        linearProbingWithReplacement.resize(tableSize);
-        compProb = 0;
-        compWithRepl = 0;
+
+    Hashtable(int tableSize) {
+        this->tableSize = tableSize;
+        clients = new Client[ tableSize ];
     }
 
-    // without replacement
-    void insertProbing(string cname, string no) {
-    	int sum = 0;
-    	for(int i = 0;i<cname.size();i++){
-    		sum += cname[i];
-    	}
-        int idx = hash(sum);
-        int i = idx;
-        compProb++;
+    void createTable() {
+        for(int i=0; i<tableSize; i++) 
+            clients[i] = Client();
+    }
 
-        while (linearProbing[i].cname != "") {
-            i = (i + 1) % tableSize;
-            compProb++;
+    void insertClient_woReplacement( Client newClient ) {
+        int index = hashfunction( newClient.name );
+        if( clients[index].isEmpty() ) {
+            clients[index] = newClient;
         }
 
-        linearProbing[i] = {cname, no};
+        else {
+            int currentIndex = index;
+            while( !clients[currentIndex].isEmpty() ) {
+                currentIndex = (currentIndex + 1) % tableSize;
+            }
+            clients[currentIndex] = newClient;
+        }
     }
 
-    // with replacement
-    void insertProbingWithRepl(string cname, string no) {
-    	int sum = 0;
-		for(int i = 0;i<cname.size();i++){
-			sum += cname[i];
+    void insertClient_wReplacement( Client newClient ) {
+        int index = hashfunction( newClient.name );
+        if( clients[index].isEmpty() ) {
+            clients[index] = newClient;
+        }
+
+        else {
+            if( isOriginal(index) ) {
+                int currentIndex = index;
+                while( !clients[currentIndex].isEmpty() ) {
+                    currentIndex = (currentIndex + 1) % tableSize;
+                }
+                clients[currentIndex] = newClient;
+            }
+
+            else {
+                Client existingClient = clients[index];
+                clients[index] = newClient;
+                int currentIndex = index;
+                while( !clients[currentIndex].isEmpty() ) {
+                    currentIndex = (currentIndex + 1) % tableSize;
+                }
+                clients[currentIndex] = existingClient;
+            }
+        }
+    }
+
+    void search( char clientName[] ) {
+        int index = hashfunction( clientName );
+        if( strcmp( clientName, clients[index].name ) == 0) {
+            cout<<"Client exists"<<endl;
+        }
+
+        else {
+
+            int currentIndex = index;
+            int passes = 0;
+            while( strcmp( clients[currentIndex].name, clientName ) != 0 && passes != tableSize ) {
+                currentIndex = (currentIndex + 1) % tableSize;
+                passes++;
+            }
+
+            if( passes != tableSize ) {
+                // clients[index].printDetails();
+                cout<<"Client exists"<<endl;
+            }
+
+            else {
+                cout<<"No such client found"<<endl;
+            }
+        }
+    }
+
+    void displayHT() {
+        cout.width( 10 ) ;
+		cout << "Client Name  " ;
+		cout.width( 10 ) ;
+		cout << "Telephone  \n";
+		for( int i = 0 ; i < (this->tableSize) ; i++ ) {
+			clients[ i ].printDetails() ;
 		}
-		int idx = hash(sum);
-        int i = idx;
-        compWithRepl++;
-
-        while (linearProbingWithReplacement[i].cname != "" && linearProbingWithReplacement[i].cname != cname) {
-            i = (i + 1) % tableSize;
-            compWithRepl++;
-        }
-
-        linearProbingWithReplacement[i] = {cname, no};
-    }
-
-    string getDataProbing(string cname) {
-    	int cmp = 0;
-    	int sum = 0;
-		for(int i = 0;i<cname.size();i++){
-			sum += cname[i];
-		}
-		int idx = hash(sum);
-        int i = idx;
-        cmp++;
-
-        while (linearProbing[i].cname != "") {
-            if (linearProbing[i].cname == cname)
-            	cout << "Found in " << cmp << "comparisons" << endl;
-                return linearProbing[i].phoneNo;
-            i = (i + 1) % tableSize;
-            cmp++;
-        }
-
-        return "Not Found";
-    }
-
-    string getDataWithRepl(string cname) {
-    	int cmp = 0;
-    	int sum = 0;
-		for(int i = 0;i<cname.size();i++){
-			sum += cname[i];
-		}
-		int idx = hash(sum);
-        int i = idx;
-        cmp++;
-        while (linearProbingWithReplacement[i].cname != "") {
-            if (linearProbingWithReplacement[i].cname == cname)
-            	cout << "Found in " << cmp << "comparisons" << endl;
-                return linearProbingWithReplacement[i].phoneNo;
-            i = (i + 1) % tableSize;
-            cmp++;
-        }
-
-        return "Not Found";
-    }
-
-    void compRequired() {
-        cout << "Comparison For inserting without replacement: " << compProb << endl;
-        cout << "Comparison For inserting with Replacement: " << compWithRepl << endl;
-    }
-
-    void displayData() {
-        cout << "With replacement" << endl;
-        for (int i = 0; i < tableSize; i++) {
-            if (linearProbingWithReplacement[i].cname != "")
-                cout << linearProbingWithReplacement[i].cname << "  " << linearProbingWithReplacement[i].phoneNo << endl;
-        }
-
-        cout << "Without replacement" << endl;
-        for (int i = 0; i < tableSize; i++) {
-            if (linearProbing[i].cname != "")
-                cout << linearProbing[i].cname << "  " << linearProbing[i].phoneNo << endl;
-        }
     }
 };
 
-void display() {
-    cout << "1. Insert without replacement\n";
-    cout << "2. Insert with replacement\n";
-    cout << "3. Get without replacement\n";
-    cout << "4. Get with replacement\n";
-    cout << "5. Replacement count\n";
-    cout << "6. Display data\n";
-    cout << "7. END\n";
+
+int main()
+{
+    // int n;
+    // cout << "Enter number of clients: ";
+    // cin >> n;
+    Hashtable table(10);
+    table.createTable();
+    // for(int i = 0; i<n; i++) {
+    //     cout << "Enter client name: ";
+    //     char name[20];
+    //     cin >> name;
+    //     cout << "Enter client phone: ";
+    //     ll phone;
+    //     cin >> phone;
+    //     Client newClient( name, phone );
+    //     table.insertClient_woReplacement(newClient);
+    // }
+
+    // table.insertClient_woReplacement(Client("Damiano", 8979926863));
+    // table.insertClient_woReplacement(Client("Muirne", 8637920981));
+    // table.insertClient_woReplacement(Client("Kristen", 5822301507));
+    // table.insertClient_woReplacement(Client("Leto", 8427943825));
+    // table.insertClient_woReplacement(Client("Anita", 5189486425));
+    // table.insertClient_woReplacement(Client("Pacífica", 9223409937));
+    // table.insertClient_woReplacement(Client("Emma", 5869973957));
+    // table.insertClient_woReplacement(Client("Bellamy", 7911222765));
+    // table.insertClient_woReplacement(Client("Ríoghnach", 9576492062));
+    // table.insertClient_woReplacement(Client("Mathis", 7892997404));
+
+    // table.displayHT();
+
+	// table.search( "Damiano" ) ;
+	// table.search( "zdfg" ) ;
+	// table.search( "asdf" ) ;
+	// table.search( "Muirne" ) ;
+	// table.search( "qwer" ) ;
+	// table.search( "Emma" ) ;
+	// table.search( "ergh" ) ;
+	// table.search( "Mathis" ) ;
+	// table.search( "zasd" ) ;
+	// table.search( "Anita" ) ;
+
+    table.insertClient_woReplacement( Client( "abcd" , 10002 ) ) ;
+	table.insertClient_woReplacement( Client( "zdfg" , 10003 ) ) ;
+	table.insertClient_woReplacement( Client( "asdf" , 10004 ) ) ;
+	table.insertClient_woReplacement( Client( "dbca" , 10005 ) ) ;
+	table.insertClient_woReplacement( Client( "qwer" , 10006 ) ) ;
+	table.insertClient_woReplacement( Client( "rtyu" , 10006 ) ) ;
+	table.insertClient_woReplacement( Client( "ergh" , 10006 ) ) ;
+	table.insertClient_woReplacement( Client( "fghj" , 10006 ) ) ;
+	table.insertClient_woReplacement( Client( "zasd" , 10006 ) ) ;
+	table.insertClient_woReplacement( Client( "dfgh" , 10006 ) ) ;
+	table.displayHT() ;
+	table.search( "abcd" ) ;
+	table.search( "zdfg" ) ;
+	table.search( "asdf" ) ;
+	table.search( "dbca" ) ;
+	table.search( "qwer" ) ;
+	table.search( "rtyu" ) ;
+	table.search( "ergh" ) ;
+	table.search( "fghj" ) ;
+	table.search( "zasd" ) ;
+	table.search( "dfgh" ) ;
+
+    Hashtable table1(10);
+
+    table1.insertClient_wReplacement( Client( "abcd" , 10002 ) ) ;
+	table1.insertClient_wReplacement( Client( "zdfg" , 10003 ) ) ;
+	table1.insertClient_wReplacement( Client( "asdf" , 10004 ) ) ;
+	table1.insertClient_wReplacement( Client( "dbca" , 10005 ) ) ;
+	table1.insertClient_wReplacement( Client( "qwer" , 10006 ) ) ;
+	table1.insertClient_wReplacement( Client( "rtyu" , 10006 ) ) ;
+	table1.insertClient_wReplacement( Client( "ergh" , 10006 ) ) ;
+	table1.insertClient_wReplacement( Client( "fghj" , 10006 ) ) ;
+	table1.insertClient_wReplacement( Client( "zasd" , 10006 ) ) ;
+	table1.insertClient_wReplacement( Client( "dfgh" , 10006 ) ) ;
+	table1.displayHT() ;
+	table1.search( "abcd" ) ;
+	table1.search( "zdfg" ) ;
+	table1.search( "asdf" ) ;
+	table1.search( "dbca" ) ;
+	table1.search( "qwer" ) ;
+	table1.search( "rtyu" ) ;
+	table1.search( "ergh" ) ;
+	table1.search( "fghj" ) ;
+	table1.search( "zasd" ) ;
+	table1.search( "dfgh" ) ;
+
+    return 0;
 }
-
-int main() {
-    DataBase b;
-
-    int choice;
-    while (true) {
-        display();
-        cin >> choice;
-        string cname;
-        string no;
-        switch (choice) {
-            case 1:
-                cout << "Enter clientName: " << endl;
-                cin >> cname;
-                cout << "Enter contact no: " << endl;
-                cin >> no;
-                b.insertProbing(cname, no);
-                cout << "Done\n";
-                break;
-            case 2:
-                cout << "Enter clientName: " << endl;
-                cin >> cname;
-                cout << "Enter contact no: " << endl;
-                cin >> no;
-                b.insertProbingWithRepl(cname, no);
-                cout << "DONE" << endl;
-                break;
-            case 3:
-                cout << "Enter clientName: " << endl;
-                cin >> cname;
-                cout << b.getDataProbing(cname) << endl;
-                break;
-            case 4:
-                cout << "Enter clientName: " << endl;
-                cin >> cname;
-                cout << b.getDataWithRepl(cname)<<endl;
-                break;
-            case 5:
-                b.compRequired();
-                break;
-            case 6:
-                b.displayData();
-                break;
-            case 7:
-                cout << "ENDING..." << endl;
-                return 0;
-            default:
-            	cout << "Enter correct choice.." << endl;
-            	break;
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
